b555d8d5347cc25a826e9b1be229def20944e4b4
Recho <springworder@gmail.com>
2018-03-23 15:23:43 +0800

    这里有一个问题，主线程不能捕获其他线程的异常

--- Program.cs
+++ Program.cs
@@ -12,16 +12,34 @@ namespace AboutAsyncAwait
 
             try
             {
-                new Thread(Todo).Start();
+                var task = Task.Run(() => { Todo(); });
+                task.Wait();
+
+                var strTask = Task.Run(() => Get());
+                var result = strTask.Result;
             }
             catch (Exception e)
             {
                 Console.WriteLine($"{e.Message}");
             }
+
+            Console.ReadKey();
         }
 
         //关于线程执行过程的异常
         static void Todo()
+        {
+            try
+            {
+                throw null;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+        }
+
+        static string Get()
         {
             throw null;
         }
