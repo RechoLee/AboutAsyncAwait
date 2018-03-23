d5afc6e7fd74367f7d9ed443fd7713634179a40f
Recho <springworder@gmail.com>
2018-03-23 15:01:56 +0800

    主线程无法捕捉Thread线程中的异常

--- Program.cs
+++ Program.cs
@@ -6,33 +6,24 @@ namespace AboutAsyncAwait
 {
     class Program
     {
-        //共享资源
-        private static string shareRes="ss";
-        /// <summary>
-        /// 信号量 用于不同线程对共享资源的访问
-        /// </summary>
-        private static Semaphore _sem = new Semaphore(2,3);//初始允许进入的线程数量，最大允许多少个线程同时访问共享资源
+
         static void Main(string[] args)
         {
-            for (int i = 1; i <9; i++)
+
+            try
             {
-                new Thread(Enter).Start(i);
+                new Thread(Todo).Start();
             }
-            Console.ReadKey();
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
         }
 
-        static void Enter(object id)
+        //关于线程执行过程的异常
+        static void Todo()
         {
-            Console.WriteLine($"{id}号开始排队。。。");
-            _sem.WaitOne();
-
-            //共享的代码片段，需要用信号量控制
-            Console.WriteLine($"{id}号进入了共享区");
-            Thread.Sleep(2000);
-
-            //出去时候释放
-            _sem.Release();
-            Console.WriteLine($"{id}执行完成");
+            throw null;
         }
     }
 }
