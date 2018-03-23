cae02b519c4a1d63efc2ce8a9727a63eaae44799
Recho <springworder@gmail.com>
2018-03-23 11:56:39 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AboutAsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(Todo).Start();//.Net 1.0就有
+
+            Task.Factory.StartNew(Todo); //.Net4.0引入TPl
+
+            Task.Run(new Action(Todo));//.Net 4.5新增
+
+        }
+
+        public static void Todo()
+        {
+            Console.WriteLine("todo something");
+        }
+    }
+}
