0ddd78043c64ee1f08f84794928daeeee3bf0216
Recho <springworder@gmail.com>
2018-03-23 13:41:04 +0800

    参数的使用

--- Program.cs
+++ Program.cs
@@ -8,9 +8,11 @@ namespace AboutAsyncAwait
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"我是主线程：id为{Thread.CurrentThread.ManagedThreadId}");
+            new Thread(Todo).Start("arg1");
 
-            ThreadPool.QueueUserWorkItem(new WaitCallback(Todo));
+            new Thread(() => { Todo("arg1", "arg2", "arg3"); }).Start();
+
+            Task.Run(() => { Todo("arg1,task","arg2","arg3"); });
 
             Console.ReadKey();
         }
@@ -18,6 +20,12 @@ namespace AboutAsyncAwait
         public static void Todo(object state)
         {
             Console.WriteLine($"我是新的线程，id为：{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public static void Todo(string arg1, string arg2, string arg3)
+        {
+            //Todo
+            Console.WriteLine($"{arg1} {arg2} {arg3}");
         }
     }
 }
