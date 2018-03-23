049835bc799bf149fc2d150eb939cd08b623bcf4
Recho <springworder@gmail.com>
2018-03-23 14:05:11 +0800

    同时操作共享数据

--- Program.cs
+++ Program.cs
@@ -6,41 +6,23 @@ namespace AboutAsyncAwait
 {
     class Program
     {
+        private static bool _isDone = false;
+
         static void Main(string[] args)
         {
-            new Thread(Todo).Start("arg1");
-
-            new Thread(() => { Todo("arg1", "arg2", "arg3"); }).Start();
-
-            Task.Run(() => { Todo("arg1,task","arg2","arg3"); });
-
-            //Thread是不能返回值的
-            //使用Task
-            var returnStr = Task.Run<string>(() => Todo("return"));
-
-            Console.WriteLine("返回值之前");
-
-            Console.WriteLine($"返回值是：{returnStr.Result}");
-
+            new Thread(Todo).Start();
+            new Thread(Todo).Start();
 
             Console.ReadKey();
         }
 
-        public static void Todo(object state)
+        public static void Todo()
         {
-            Console.WriteLine($"我是新的线程，id为：{Thread.CurrentThread.ManagedThreadId}");
-        }
-
-        public static void Todo(string arg1, string arg2, string arg3)
-        {
-            //Todo
-            Console.WriteLine($"{arg1} {arg2} {arg3}");
-        }
-
-        public static string Todo(string arg1)
-        {
-            Thread.Sleep(2000);
-            return arg1;
+            if (!_isDone)
+            {
+                _isDone = true;
+                Console.WriteLine($"执行：{_isDone}");
+            }
         }
     }
 }
