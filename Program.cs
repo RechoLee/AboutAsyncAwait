94365245ffe023339058ee5f5ab8f8c6d0b95a54
Recho <springworder@gmail.com>
2018-03-23 14:56:47 +0800

    通过信号量来控制线程的访问

--- Program.cs
+++ Program.cs
@@ -6,28 +6,33 @@ namespace AboutAsyncAwait
 {
     class Program
     {
-        private static bool _isDone = false;
-
-        private static object _lock = new object();
-
+        //共享资源
+        private static string shareRes="ss";
+        /// <summary>
+        /// 信号量 用于不同线程对共享资源的访问
+        /// </summary>
+        private static Semaphore _sem = new Semaphore(2,3);//初始允许进入的线程数量，最大允许多少个线程同时访问共享资源
         static void Main(string[] args)
         {
-            new Thread(Todo).Start();
-            new Thread(Todo).Start();
-
+            for (int i = 1; i <9; i++)
+            {
+                new Thread(Enter).Start(i);
+            }
             Console.ReadKey();
         }
 
-        public static void Todo()
+        static void Enter(object id)
         {
-            //线程加锁  独占锁 在一个线程未执行完成之前别的线程不能访问
+            Console.WriteLine($"{id}号开始排队。。。");
+            _sem.WaitOne();
 
-            lock(_lock){
-                _isDone = true;
-                Console.WriteLine($"执行：{_isDone}");
-                Thread.Sleep(2000); //等待后 这里会执行两次 线程不安全
+            //共享的代码片段，需要用信号量控制
+            Console.WriteLine($"{id}号进入了共享区");
+            Thread.Sleep(2000);
 
-            }
+            //出去时候释放
+            _sem.Release();
+            Console.WriteLine($"{id}执行完成");
         }
     }
 }
